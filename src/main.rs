7606e1691f532310b8ec57e6f04891a048d14f61
Mikhail Fedyakov <mf_52@mail.ru>
2022-01-15 23:34:26 +0300

    better?

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use std::thread::sleep;
 use std::time::Duration;
 use crate::single_thread_executor::Executor;
 
@@ -6,7 +7,10 @@ mod single_thread_executor;
 
 fn main() {
     let mut executor = Executor::new();
-    let mut f = || -> () {println!("Hello!")};
+    let mut f = || -> () {println!("Hello!1")};
     executor.append(Box::new(f));
+    sleep(Duration::from_secs(1));
+    let mut f = || -> () {println!("Hello!2")};
+    sleep(Duration::from_secs(1));
 }
 
--- src/single_thread_executor.rs
+++ src/single_thread_executor.rs
@@ -1,10 +1,11 @@
 use std::collections::VecDeque;
-use std::fmt::{Display, Formatter};
+// use std::fmt::{Display, Formatter};
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 
+#[derive(Debug, Clone, Copy)]
 enum State {
-    NEW,
+    // NEW,
     RUNNING,
     STOPPED,
 }
@@ -18,38 +19,48 @@ pub struct Executor {
     executor: Option<thread::JoinHandle<()>>,
     state: State,
     cv: Condvar,
-    queue: Arc<Mutex<VecDeque<Box<dyn Fn()>>>>,
+    queue: Arc<Mutex<VecDeque<Box<dyn Fn() + Send + 'static>>>>,
 }
 
 impl Executor {
     pub fn new() -> Executor {
         Executor {
-            executor: None,
-            state: State::NEW,
+            executor: Some(
+                thread::spawn(|| {
+                    loop {}
+                })
+            ),
+            state: State::RUNNING,
             cv: Condvar::new(),
             queue: Arc::new(Mutex::new(VecDeque::new())),
         }
     }
-    
-    pub fn append(&mut self, f: Box<dyn Fn()>) -> Result<(), CustomError> {
-        let processor = || {
-            loop {
-                // let s = self.queue.clone();
-                // if self.queue.lock().unwrap().is_empty() {
-                //     // self.cv.wait(self.queue.lock().unwrap()).unwrap();
-                // } else {
-                // //     let f = self.queue.lock().unwrap().pop_back().unwrap();
-                // //     f();
-                // }
-                // println!("Some stupid words! {}", s.lock().unwrap().len());
-            }
-        };
+
+    // Dumb shit is to use dyn Fn() + Send. It's literally illogical syntax.
+    // Box<dyn Fn()> sugar for Box<dyn Fn() + 'static>
+    pub fn append(&mut self, f: Box<dyn Fn() + Send>) -> Result<(), CustomError> {
+        // let s = self.queue.clone();
+        // let s = f;
+        // let processor = move || {
+        //     loop {
+        //         // f();
+        //         // s.lock().unwrap().len();
+        //         // let s = self.queue.clone();
+        //         // if s.lock().unwrap().is_empty() {
+        //         if self.queue.lock().unwrap().is_empty() {
+        //             self.cv.wait(self.queue.lock().unwrap()).unwrap();
+        //         } else {
+        //         //     let f = self.queue.lock().unwrap().pop_back().unwrap();
+        //         //     f();
+        //         }
+        //         // println!("Some stupid words! {}", s.lock().unwrap().len());
+        //     }
+        // };
         match self.state {
-            State::NEW => {
-                self.executor = Some(thread::spawn(processor));
-                self.state = State::RUNNING;
-                self.queue.lock().unwrap().push_back(f);
-            }
+            // State::NEW => {
+            //     self.state = State::RUNNING;
+            //     self.queue.lock().unwrap().push_back(f);
+            // }
             State::RUNNING => {
                 self.queue.lock().unwrap().push_back(f);
                 self.cv.notify_all();
@@ -62,19 +73,19 @@ impl Executor {
     }
 }
 
-impl Display for Executor {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        todo!()
-    }
-}
+// impl Display for Executor {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//         todo!()
+//     }
+// }
+//
+// impl Drop for Executor {
+//     fn drop(&mut self) {
+//         todo!()
+//     }
+// }
 
-impl Drop for Executor {
-    fn drop(&mut self) {
-        todo!()
-    }
-}
-
-unsafe impl Send for Executor {}
+unsafe impl Sync for Executor {}
 
 #[cfg(test)]
 mod tests {
